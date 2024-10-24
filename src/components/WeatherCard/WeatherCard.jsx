3ebb02cd0a857e3e7587366cde4531e4d42d8b66
Michael B <mbostick94@yahoo.com>
2024-10-23 20:29:42 -0400

    add css imports

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -3,7 +3,7 @@ import "./WeatherCard.css"
 
 function WeatherCard() {
     return <section className="weather-card">
-        <p className="weather-card__temp">75 degrees</p>
+        <p className="weather-card__temp">75 &deg; F</p>
         <img src={sunny} alt="sunny" className="weather-card__image" />
     </section>
 }
